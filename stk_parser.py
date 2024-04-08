7ca1c43af7ecddee72553d4b7eb4f8c021e1d882
nst <okolotec@gmail.com>
2024-04-08 08:36:12 +0500

    full detection instead of 500

--- stk_parser.py
+++ stk_parser.py
@@ -12,8 +12,8 @@ load_dotenv(dotenv_path)
 STK_LOGIN = os.environ.get('STK_LOGIN')
 STK_PASSWORD = os.environ.get('STK_PASSWORD')
 STK_ACCESS_TOKEN = ''
-PREMOD_URL = 'http://fku-ural.stk-drive.ru/api/detections/premoderation-room/'
-VALIDATION_STATUS = 'VALID_DETECTION'
+PREMOD_URL = 'http://fku-ural.stk-drive.ru/api/detections/'
+VALIDATION_STATUS = 'AWAITING_VALIDATION'# 'VALID_DETECTION'
 # while True:
 STK_ACCESS_TOKEN = json.loads(
     requests.post(
@@ -22,9 +22,12 @@ STK_ACCESS_TOKEN = json.loads(
         ).text
     ).get("access")
 
+
+
+
 res = json.loads(
     requests.get(
-        url= f'{PREMOD_URL}?validation_status={VALIDATION_STATUS}&created_at__gte=2024-04-03T19:00:00.000Z&created_at__lte=2024-04-30T18:59:59.999Z',
+        url= f'{PREMOD_URL}?validation_status={VALIDATION_STATUS}&created_at__gte=2024-03-31T19:00:00.000Z',
         headers={'Authorization': f'Bearer {STK_ACCESS_TOKEN}'},
         verify=False
     ).text
@@ -34,6 +37,4 @@ for el in res:
     data.append(
         {el.get('id'):f'{(el.get("created_at"))[5:-13]}'}
     )
-print(len(res))
-print(data[0],data[int(len(data)/2)],data[len(data)-1])
-#   time.sleep(5)
+print(len(data))
--- tg_bot.py
+++ tg_bot.py
@@ -1,7 +1,21 @@
 import asyncio
 import aiogram
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-def send_message():
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters.command import Command
+from aiogram.types import Message
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+
+def main():
     pass
+
+
+
+if __name__ == '__main__':
+    print("\t> live")
+    asyncio.run(main())

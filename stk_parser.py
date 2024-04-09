0b4d76d14b4e7048205495b92eaa1e3179ff3aab
nst <okolotec@gmail.com>
2024-04-09 15:40:42 +0500

    make stk_parser async

--- stk_parser.py
+++ stk_parser.py
@@ -1,8 +1,9 @@
-import requests
+import aiohttp
+import asyncio
 import json
 import time
 import os
-import asyncio
+
 
 from datetime import date, time, datetime
 from os.path import join, dirname
@@ -18,26 +19,35 @@ PREMOD_URL = 'http://fku-ural.stk-drive.ru/api/detections/'
 
 async def get_stk_token() -> str:
     '''возвращает токен по логин:паролю '''
-    return json.loads(
-                requests.post(
-                    url='http://fku-ural.stk-drive.ru/api/users/token/',
-                    data={"username": STK_LOGIN, "password": STK_PASSWORD}
-                    ).text
-                ).get("access")
+    
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                url='http://fku-ural.stk-drive.ru/api/users/token/',
+                data={"username": STK_LOGIN, "password": STK_PASSWORD}
+            ) as response:
+            return json.loads(str(await response.text())).get("access")
+    
 
-async def get_detections(token=get_stk_token(), status='AWAITING_VALIDATION',created_gte='2024-03-31') -> list[dict]:
+async def get_detections(token=None, status='AWAITING_VALIDATION',created_gte='2024-03-31') -> list[dict]:
     '''под токеном получает детекции с created_gte даты по настоящее время, возвращает список словарей id:created_at'''
-    response = requests.get(
-            url= f'{PREMOD_URL}?validation_status={status}&created_at__gte={created_gte}T19:00:00.000Z',
-            headers={'Authorization': f'Bearer {token}'},
-            verify=False)
-    if response.status_code != 200:
-        print(response.status_code, response.text, token)
-        return response.status_code
-    # формирует data [{id: created_at}, ...] из response
-    return [{el.get('id'):f'{(el.get("created_at"))[5:-13]}'} for el in json.loads(response.text)] 
+    if not token:
+        token = await get_stk_token()
 
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                url= f'{PREMOD_URL}?validation_status={status}&created_at__gte={created_gte}T19:00:00.000Z',
+                headers={'Authorization': f'Bearer {token}'}
+            ) as response:
+            if response.status != 200:
+                print(response.status, await response.text(), token)
+                return response.status
+            # формирует data [{id: created_at}, ...] из response
+            return [{el.get('id'):f'{(el.get("created_at"))[5:-13]}'} for el in json.loads(await response.text())] 
+
+    
 if __name__ == "__main__":
     print('from stk_parser.py')
-    print(get_detections())
-    print(len(get_detections()))
+    d = asyncio.run(get_detections())
+    print(len(d))
+    

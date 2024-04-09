137c700d0ab06bff7600c9cba25a0cec58fa09df
nst <okolotec@gmail.com>
2024-04-09 22:13:55 +0500

    some refactoring

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import time
 import asyncio
 from datetime import  datetime
@@ -6,16 +5,26 @@ from stk_parser import get_detections, get_stk_token
 
 #TODO remake funtional to OOP
 #     try catch
+#     change prints to tg_bot module
+
 
 async def main():
+
+    '''take [detections] from stk_parser, manipulate and use tg_bot to send it and'''
+
     token = await get_stk_token()
+
     while True:
         status, d = await get_detections(token)
         if status != 200:
             token = await get_stk_token()
-            print(f'new token: ..{token[:-10]}')
+
+            print(f'taken new token: ..{token[:-10]}')
+
             continue
-        print(f'{datetime.now().hour}:{datetime.now().minute} - awaiting {len(d)}')
+
+        print(f'{datetime.now().strftime("%H:%M:%S")} - awaiting {len(d)}')
+
         await asyncio.sleep(40)
 
 asyncio.run(main())
--- stk_parser.py
+++ stk_parser.py
@@ -1,44 +1,50 @@
+from typing import Any
 import aiohttp
-import asyncio
 import json
 import os
 
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-
-dotenv_path = join(dirname(__file__),'.env')
-load_dotenv(dotenv_path)
+load_dotenv(join(dirname(__file__),'.env'))
 STK_LOGIN = os.environ.get('STK_LOGIN')
 STK_PASSWORD = os.environ.get('STK_PASSWORD')
-STK_ACCESS_TOKEN = ''
 PREMOD_URL = 'http://fku-ural.stk-drive.ru/api/detections/'
 
-async def get_stk_token():
-    '''возвращает токен по логин:паролю '''
-    async with aiohttp.ClientSession(trust_env = True) as session:
+async def get_stk_token() -> str:
+
+    '''login:pass -> beaver token'''
+
+    async with aiohttp.ClientSession(trust_env=True) as session:
         async with session.post(
                 url='http://fku-ural.stk-drive.ru/api/users/token/',
                 data={"username": STK_LOGIN, "password": STK_PASSWORD}
             ) as response:
-            return json.loads(str(await response.text())).get("access")
-    
 
-async def get_detections(token:str, status='AWAITING_VALIDATION',created_gte='2024-03-31') -> list[dict]:
-    '''под токеном получает детекции с created_gte даты по настоящее время, возвращает список словарей id:created_at'''
-    async with aiohttp.ClientSession(trust_env = True) as session:
+            return json.loads(str(await response.text())).get("access")
+
+
+async def get_detections(
+            token:str,
+            status='AWAITING_VALIDATION',
+            created_gte='2024-03-31'
+        ) -> list[Any]:
+
+    async with aiohttp.ClientSession(trust_env=True) as session:
         async with session.get(
                 url= f'{PREMOD_URL}?validation_status={status}&created_at__gte={created_gte}T19:00:00.000Z',
                 headers={'Authorization': f'Bearer {token}'}
             ) as response:
+
             res_text = await response.text()
+
             if response.status != 200:
+                # invalid request
                 return [response.status, res_text, token]
-            data = [{el.get('id'):f'{(el.get("created_at"))[5:-13]}'} for el in json.loads(res_text)]
+
+            data = []
+            for el in json.loads(res_text):
+                # make small [{id:created_at}, {..}, ..] from big [response with all information]
+                data.append({el.get('id'):f'{(el.get("created_at"))[5:-13]}'})
+
             return [response.status, data]
-    
-if __name__ == "__main__":
-    print('from stk_parser.py')
-    d = asyncio.run(get_detections())
-    print(len(d))
-    

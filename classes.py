0406d892b41ef61b0c6b68fb78f78412a1d132a7
nst <okolotec@gmail.com>
2024-05-02 17:04:01 +0500

    logging and command-line argumenst

--- classes.py
+++ classes.py
@@ -15,17 +15,14 @@ from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message, InlineQuery, InlineQueryResultArticle,InputTextMessageContent
 
 
-load_dotenv(
-    find_dotenv('.env', raise_error_if_not_found=True),
-    verbose=True)
 
 @dataclass
 class Parser():
 
-    login: str = os.environ.get('STK_LOGIN', '')
-    passw: str = os.environ.get('STK_PASSWORD', '')
-    d_url = 'http://fku-ural.stk-drive.ru/api/detections/'
-    t_url = 'http://fku-ural.stk-drive.ru/api/users/token/'
+    login: str
+    passw: str
+    detections_url = 'http://fku-ural.stk-drive.ru/api/detections/'
+    token_url = 'http://fku-ural.stk-drive.ru/api/users/token/'
 
 
     async def get_token(
@@ -33,7 +30,7 @@ class Parser():
             stk_login: str = '') -> str:
         async with http(trust_env=True) as session:
             async with session.post(
-                url = self.t_url,
+                url = self.token_url,
                 data = {
                     'username': self.login,
                     'password': self.passw
@@ -60,7 +57,7 @@ class Parser():
         ) -> list[dict]:
         async with http(trust_env=True) as session:
             async with session.get(
-                url=f'{self.d_url}?'
+                url=f'{self.detections_url}?'
                     f'validation_status={status}&'+
                     f'created_at__gte={created_gte}T00:00:00.000Z',
                 headers={
@@ -93,26 +90,3 @@ class TelegramBot():
         pass
     async def start_handler(self):
         pass
-
-
-
-
-
-async def main():
-    p = Parser()
-    b = TelegramBot()
-    await b.connect()
-    token = await p.get_token()
-    detections = await p.get_all_detections(token=token)
-    print(len(detections))
-
-
-
-if __name__ == '__main__':
-    print(sys.argv)
-    logging.basicConfig(
-        level=(logging.DEBUG if sys.argv[0]=='d' else logging.INFO)
-    )
-    print(f'{datetime.now().strftime("%X.%f")[:-3]} | SYS | start from clasess.py')
-    asyncio.run(main())
-
--- main.py
+++ main.py
@@ -1,15 +1,69 @@
+import argparse
 import asyncio
+import logging
+import os
 from datetime import datetime
+
+from dotenv import find_dotenv, load_dotenv
 
 from classes import Parser
 
-p = Parser()
-async def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async def main(*args):
+
     token = await p.get_token()
     d = await p.get_all_detections(token=token)
     print(len(d))
 
 
 if __name__ == '__main__':
+    bot_token = os.environ.get('TEST_TOKEN', '0')
+    stk_login: str = os.environ.get('STK_LOGIN', '')
+    stk_passw: str = os.environ.get('STK_PASSWORD', '')
+
     print( f'{datetime.now().strftime("%X.%f")[:-3]} | SYS | start from beta main.py')
-    asyncio.run(main())
+
+
+
+    # command-line arguments
+    parser = argparse.ArgumentParser(
+
+    )
+    parser.add_argument('--log-level',
+                           default='INFO',
+                           choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'))
+
+    parser.add_argument('--test-bot')
+    options = parser.parse_args()
+
+    # logging
+    logging.basicConfig(
+        format='%(levelname)s %(name)s %(message)s',
+        level=options.log_level
+    )
+    logger = logging.getLogger(__name__)
+
+
+
+    print(logger.level)
+    print(f'{os.path.basename(__file__)}')
+    asyncio.run(main(
+        bot_token, stk_login, stk_passw
+    ))

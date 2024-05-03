a83af28b75ba8fe70f81d37ff087e42b6689dfec
nst <okolotec@gmail.com>
2024-05-03 16:55:36 +0500

    class for argparse

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@ import os
 import sys
 import aiogram
 import logging
+import argparse
 
 from dataclasses import dataclass
 from datetime import datetime
@@ -11,86 +12,91 @@ from json import loads
 import aiogram
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandObject, CommandStart
-from aiogram.types import (InlineQuery, InlineQueryResultArticle,
-                           InputTextMessageContent, Message)
+from aiogram.types import (
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message,
+)
 from aiohttp import ClientSession as http
-from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(
-    find_dotenv('.envb', raise_error_if_not_found=True),
-    verbose=True)
 
 @dataclass
-class Parser():
+class DetectionsParser:
 
     login: str
     passw: str
-    detections_url = 'http://fku-ural.stk-drive.ru/api/detections/'
-    token_url = 'http://fku-ural.stk-drive.ru/api/users/token/'
+    detections_url = "http://fku-ural.stk-drive.ru/api/detections/"
+    token_url = "http://fku-ural.stk-drive.ru/api/users/token/"
 
-
-    async def get_token(
-            self,
-            stk_login: str = '') -> str:
+    async def get_token(self, stk_login: str = "") -> str:
         async with http(trust_env=True) as session:
             async with session.post(
-                url = self.token_url,
-                data = {
-                    'username': self.login,
-                    'password': self.passw
-                }
+                url=self.token_url,
+                data={"username": self.login, "password": self.passw},
             ) as response:
                 if response.status == 200:
                     print(f'{datetime.now().strftime("%X.%f")[:-3]} | STK | get token')
-                    return loads(
-                        str(
-                            await response.text()
-                        )
-                    ).get('access')
-                else: # Exceptions
-                    raise Exception(f'\n{datetime.now().strftime("%X.%f")[:-3]} | STK | get_token {response.status} :\n'
-                                    f'creds    | {self.login}:{self.passw}\n'
-                                    f'response | {await response.text()}')
-
+                    return loads(str(await response.text())).get("access")
+                else:  # Exceptions
+                    raise Exception(
+                        f'\n{datetime.now().strftime("%X.%f")[:-3]} | STK | get_token {response.status} :\n'
+                        f"creds    | {self.login}:{self.passw}\n"
+                        f"response | {await response.text()}"
+                    )
 
     async def get_all_detections(
-            self,
-            token: str,
-            status: str = 'AWAITING_VALIDATION',
-            created_gte: str ='2024-04-01'
-        ) -> list[dict]:
+        self,
+        token: str,
+        status: str = "AWAITING_VALIDATION",
+        created_gte: str = "2024-04-01",
+    ) -> list[dict]:
         async with http(trust_env=True) as session:
             async with session.get(
-                url=f'{self.detections_url}?'
-                    f'validation_status={status}&'+
-                    f'created_at__gte={created_gte}T00:00:00.000Z',
-                headers={
-                    'Authorization': f'Bearer {token}'
-                }
+                url=f"{self.detections_url}?"
+                f"validation_status={status}&"
+                + f"created_at__gte={created_gte}T00:00:00.000Z",
+                headers={"Authorization": f"Bearer {token}"},
             ) as response:
                 if response.status == 200:
-                    print(f'{datetime.now().strftime("%X.%f")[:-3]} | STK | get detections')
-                    return loads(
-                        str(
-                            await response.text()
-                        )
+                    print(
+                        f'{datetime.now().strftime("%X.%f")[:-3]} | STK | get detections'
                     )
-                else: # Exceptions
-                    raise Exception(f'{datetime.now().strftime("%X.%f")[:-3]} | STK | get_detections{response.status}:\n'
-                                    f'    | creds: {self.login}:{self.passw}\n'
-                                    f'    | resp: {await response.text()}')
+                    return loads(str(await response.text()))
+                else:  # Exceptions
+                    raise Exception(
+                        f'{datetime.now().strftime("%X.%f")[:-3]} | STK | get_detections{response.status}:\n'
+                        f"    | creds: {self.login}:{self.passw}\n"
+                        f"    | resp: {await response.text()}"
+                    )
 
 
-class TelegramBot():
+class TelegramBot:
     def __init__(self) -> None:
         pass
 
     async def connect(self):
-        BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+        BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
         print(len(BOT_TOKEN))
         bot = aiogram.Bot(BOT_TOKEN)
 
     async def create_dispatcher(self):
         pass
+
     async def start_handler(self):
         pass
+
+
+class CMDLineArguments(argparse.ArgumentParser):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.add_argument(
+            "--log-level",
+            default="ERROR",
+            choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+        )
+
+        self.add_argument(
+            "-b", "--test-bot", help="start with TEST_TOKEN env", action="store_true"
+        )
--- main.py
+++ main.py
@@ -6,42 +6,22 @@ from datetime import datetime
 
 from dotenv import find_dotenv, load_dotenv
 
-from classes import Parser
+from classes import CMDLineArguments, DetectionsParser, TelegramBot
 
 
 async def main():
+    # load_dotenv(find_dotenv(raise_error_if_not_found=True), verbose=True)
 
-    print( f'{datetime.now().strftime("%X.%f")[:-3]} | SYS | start from beta main.py')
-
-
-
-    # command-line arguments
-    parser = argparse.ArgumentParser(
-
-    )
-    parser.add_argument('--log-level',
-                           default='INFO',
-                           choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'))
-
-    parser.add_argument('--test-bot')
-    options = parser.parse_args()
+    p = CMDLineArguments()
+    options = p.parse_args()
 
     # logging
-    logging.basicConfig(
-        format='%(levelname)s %(name)s %(message)s',
-        level=options.log_level
-    )
     logger = logging.getLogger(__name__)
-
-
-
+    logger.setLevel(options.log_level)
+    print(options)
     print(logger.level)
-    print(f'{os.path.basename(__file__)}')
+    print(f"{os.path.basename(__file__)}")
 
 
-if __name__ == '__main__':
-
-
-
-
+if __name__ == "__main__":
     asyncio.run(main())
